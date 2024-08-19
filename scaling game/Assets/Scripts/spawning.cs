c2cb06418f6e5edff41f46d37f9bebcc8fd15f8b
MAbubakarI <Abustar421@gmail.com>
2024-08-19 14:37:24 -0600

    spawner

--- scaling game/Assets/Scripts/spawning.cs
+++ scaling game/Assets/Scripts/spawning.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class spawning : MonoBehaviour
 {
+
+    public GameObject groundPrefab;
+    public Vector2 mousepos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mousepos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousepos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(groundPrefab, mousepos, Quaternion.identity);
+        }
     }
 }
