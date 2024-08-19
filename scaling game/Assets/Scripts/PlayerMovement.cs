3c5c464a024ecc652528d67b698329c9bee01def
Big ok <anthonyyunli@gmail.com>
2024-08-19 14:21:27 -0600

    fixed jumping

--- scaling game/Assets/Scripts/PlayerMovement.cs
+++ scaling game/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ public class NPlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private bool grounded;
 
     private void Awake()
     {
@@ -13,9 +14,27 @@ public class NPlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput > 0.01f)
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
